10cde110120dba84ea5740f5450ac1cc25a0081c
Gabriel Martins <gmapwebdev@gmail.com>
2024-06-17 21:36:05 -0300

    Commit 1

---
+++ Introduction/src/introduction/Sellintroduction.java
@@ -0,0 +1,25 @@
+package introduction;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class Sellintroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Invoking Browser
+		// Chrome - ChromeDriver -->Methods
+		//Firefox - FirefoxDrover -- Methods close get
+		//Safari - SafariDriver --> Methods close get
+		//Webdriver methods + class methods
+		
+		// Chromedriver.exe --> Chrome Browser
+		// step to invoke chrome driver //
+		//Selenium Manager
+		WebDriver driver = new ChromeDriver ();
+		
+
+	}
+
+}
